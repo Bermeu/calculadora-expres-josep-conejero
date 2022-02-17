b790cfe0eabe54903f4b0291171493c4330040c5
Josep Conejero <bermeu@gmail.com>
2022-02-17 15:16:28 +0100

    Adden app.get

--- server/index.js
+++ server/index.js
@@ -1,24 +1,22 @@
-const http = require("http");
 const debug = require("debug")("calculator:root");
 const chalk = require("chalk");
 const url = require("url");
 const express = require("express");
+const morgan = require("morgan");
 
 const initializeServer = (port) => {
   const app = express();
   app.listen(port, () => {
     debug(chalk.green(`Servidor en marcha en http://localhost:${port}`));
-    // resolve();
+  });
 
-    /* new promise ((resolve, reject) =>{ 
-server.listen(port, () => {
-    debug(chalk.green(`Servidor en marcha en http://localhost:${port}`));
-    resolve();
-  });
- server.on("request", (req, res, next) => {
-    debug(`Request llega a ${req.url} con método ${req.method}`);
-  });
-  reject(); */
+  app.use(morgan("dev"));
+
+  app.get("/kitten", (req, res, next) => {
+    debug("están pidiendo kitten");
+    debug(req.status);
+    // debug(res);
+    res.end();
   });
 
   /* app.use((req.res,next) => {
@@ -26,7 +24,11 @@ debug("ha llegado una petición a ${req.url}");
 next(); // le pasa la pelota al sgte, q siga rulando la request (pa q no se quede esperando)
 }  */
 
-  app.use();
+  app.use((err, req, res, next) => {
+    debug();
+    res.status(500);
+    res.json({ error: true, message: "se ha producicdo un error" });
+  });
 };
 
 module.exports = initializeServer;
